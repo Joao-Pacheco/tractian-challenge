8966d937da6c982f2f978cf74ef152aa1871960b
Joao Pacheco <juhkittopacheco@gmail.com>
2025-01-28 21:38:28 -0300

    feature: search of assets done

--- app/api/assets/[id]/route.ts
+++ app/api/assets/[id]/route.ts
@@ -2,68 +2,90 @@ import { NextResponse } from "next/server";
 
 async function fetchAssets(companyId: string): Promise<AssetNotCatalogued[]> {
   const API_URL = `https://fake-api.tractian.com/companies/${companyId}/assets`;
-
   const response = await fetch(API_URL);
 
   if (!response.ok) {
     throw new Error(`Failed to fetch assets. Status: ${response.status}`);
   }
-
   return response.json();
-}
-
-function assetsAdapter(item: AssetNotCatalogued) {
-  return {
-    id: item.id,
-    name: item.name,
-    locationId: item.locationId ?? null,
-  };
-}
-
-function subAssetsAdapter(item: AssetNotCatalogued) {
-  return {
-    id: item.id,
-    name: item.name,
-    parentId: item.parentId ?? null,
-  };
-}
-
-function componetsAdapter(item: AssetNotCatalogued) {
-  return {
-    id: item.id,
-    name: item.name,
-    parentId: item.parentId ?? null,
-    sensorId: item.sensorId ?? null,
-    sensorType: item.sensorType ?? null,
-    status: item.status ?? null,
-    gatewayId: item.gatewayId ?? null,
-  };
 }
 
 export async function GET(request: Request, context: any) {
   const { params } = context;
   const { id } = params;
+
   try {
-    let response = await fetchAssets(id);
+    const response = await fetchAssets(id);
 
-    let assets = <Asset[]>[];
-    let subAssets = <subAsset[]>[];
-    let componets = <Componet[]>[];
+    const componentWithoutParentOrLocation: Componet[] = [];
+    const assetsMap = new Map<string, Asset>();
+    const subAssetsMap = new Map<string, SubAsset>();
+    const components: Componet[] = [];
 
-    response.map((item) => {
-      if (item.sensorType !== null)
-        return componets.push(componetsAdapter(item));
-      if (item.locationId !== null) return assets.push(assetsAdapter(item));
-      if (item.parentId !== null) return subAssets.push(subAssetsAdapter(item));
-    });
-    //TODO: tirar o response da resposta aqui de baixo
+    for (const item of response) {
+      if (item.sensorType !== null) {
+        if (item.parentId || item.locationId) {
+          components.push(item);
+        } else {
+          componentWithoutParentOrLocation.push(item);
+        }
+      } else {
+        const key = item.parentId ? "sub" : "asset";
+        const adaptedItem =
+          key === "sub"
+            ? {
+                id: item.id,
+                name: item.name,
+                parentId: item.parentId,
+                locationId: null,
+                children: [],
+              }
+            : {
+                id: item.id,
+                name: item.name,
+                parentId: null,
+                locationId: item.locationId,
+                children: [],
+              };
+        if (key === "sub") {
+          subAssetsMap.set(item.id, adaptedItem);
+        } else {
+          assetsMap.set(item.id, adaptedItem);
+        }
+      }
+    }
+
+    for (const component of components) {
+      const parent = component.parentId
+        ? subAssetsMap.get(component.parentId) ||
+          assetsMap.get(component.parentId)
+        : assetsMap.get(component.locationId ?? "");
+
+      if (parent) {
+        parent.children.push(component);
+      }
+    }
+
+    for (const subAsset of subAssetsMap.values()) {
+      const parent = assetsMap.get(subAsset.parentId ?? "");
+      if (parent) {
+        parent.children.push(subAsset);
+      }
+    }
+
     return NextResponse.json(
-      { success: true, data: { assets, subAssets, componets, response } },
+      {
+        success: true,
+        data: {
+          assets: Array.from(assetsMap.values()),
+          componentWithoutParentOrLocation,
+          response,
+        },
+      },
       { status: 200 }
     );
   } catch (error: any) {
     console.error("Error fetching assets:", error.message);
-
     return NextResponse.json(
       {
         success: false,
--- model/assets.ts
+++ model/assets.ts
@@ -8,17 +8,18 @@ interface AssetNotCatalogued {
   gatewayId: string | null;
   locationId: string | null;
 }
-
 interface Asset {
   id: string;
   name: string;
   locationId: string | null;
+  children: Array<SubAsset | Componet>;
 }
 
-interface subAsset {
+interface SubAsset {
   id: string;
   name: string;
   parentId: string | null;
+  children: Array<SubAsset | Componet>;
 }
 
 interface Componet {
@@ -29,4 +30,13 @@ interface Componet {
   sensorType: string | null;
   status: string | null;
   gatewayId: string | null;
+  locationId: string | null;
+}
+
+interface AssetOrSubAsset {
+  id: string;
+  name: string;
+  parentId: string | null;
+  locationId: string | null;
+  children: (Asset | SubAsset | Componet)[];
 }
